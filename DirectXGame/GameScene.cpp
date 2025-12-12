653d83f2af364a7752164eeb0c488825fb16c691
BabaMasaomi <k025g0111@g.neec.ac.jp>
2025-12-12 16:01:56 +0900

    KamataEngineを入れたぞ

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,25 @@
+#include "GameScene.h"
+#include"KamataEngine.h"
+
+// KamataEngine::を毎回入力しなくてもいい様にする
+using namespace KamataEngine;
+
+// 初期化
+void GameScene::Initialize() {
+	// メンバ変数への代入処理
+
+	// インゲームの初期化処理
+
+}
+
+// 更新
+void GameScene::Update() {
+	// インゲームの更新処理
+
+}
+
+// 描画
+void GameScene::Draw() {
+	// インゲームの描画処理
+
+}
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,63 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+
+// KamataEngine::を毎回入力しなくてもいい様にする
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	Initialize(L"LC1A_25_ババ_マサオミ_AL2");
+
+	// DirectXCommonインスタンスを取得(画面描画に使用)
+	DirectXCommon* dxCoommon = DirectXCommon::GetInstance();
+
+
+	/*-------------------- インスタンス生成 --------------------*/
+	// ゲームシーン
+	GameScene* gameScene = new GameScene;
+
+
+	/*-------------------- 初期化処理 --------------------*/
+	// ゲームシーン
+	gameScene->Initialize();
+
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (Update()) {
+			break;
+		}
+
+		/*--------------- 更新処理 ---------------*/
+		// ゲームシーン
+		gameScene->Update();
+
+
+		/*--------------- 描画処理 ---------------*/
+		// 描画開始
+		dxCoommon->PreDraw();
+
+		// 描画処理を記述
+		// ゲームシーン
+		gameScene->Draw();
+
+		// 描画終了
+		dxCoommon->PostDraw();
+	}
+
+
+	// 解放処理
+	delete gameScene;
+
+	// nullputrの代入(解放処理とセットで)
+	gameScene = nullptr;
+
+	// エンジンの終了
+	Finalize();
+
+	return 0;
+}
